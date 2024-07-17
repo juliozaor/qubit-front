37e1555bdc2c3b617a067c14aff583cfb1fb57c3
kevinsoto772 <kosotosimanca@gmail.com>
2024-07-17 10:54:58 -0500

    Actualizacion crear nueva version de proyecto y actualizar version de proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://127.0.0.1:4343/api/v1/'
+  urlBackend: 'http://localhost:4343/api/v1/'
 }; 
 
   // urlBackend: 'http://127.0.0.1:4343/api/v1/''http://localhost:4343/api/v1/'
