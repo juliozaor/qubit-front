2e92d5716b4e660f9bd24f2b69d13a45345b9e78
Julio Jimenez <zaor.julio@gmail.com>
2024-08-01 15:00:47 -0500

    ajuste de environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://localhost:4343/api/v1/',
-  urlGraphic: 'http://localhost/drawio/'
+  urlBackend: 'http://127.0.0.1:4343/api/v1/',
+  urlGraphic: 'http://127.0.0.1/drawio/'
 }; 
 
   // urlBackend: 'http://127.0.0.1:4343/api/v1/''http://localhost:4343/api/v1/'
